54f781b10cef87026788cc2f3f91ee44d6f363e1
yevdev016 <curaarvey@gmail.com>
2026-04-30 18:35:17 +0800

    Configure jwt

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import jwt from 'jsonwebtoken'
 import { signUpController, signinController, googleCallback } from '../controllers/authController.js';
 import passport from 'passport';
+import { generateJwt } from '../services/authService.js';
 const router = express.Router();
 
 router.post('/signin', signinController);
@@ -12,8 +14,16 @@ router.get('/google/callback',
         session: false
     }),
     (req, res) => {
-        const token = req.user.token;
-        res.redirect(`http://localhost:5173/dashboard?token=${token}`);
+        try {
+            const userId = req.user.id;
+
+            const token = generateJwt(userId);
+            res.redirect(`http://localhost:5173/dashboard?token=${token}`);
+        } catch(err) {
+            console.error("Token Generation Error:", error);
+            res.redirect('http://localhost:5173/login?error=token_generation_failed');
+        }
+        
     }
 );
 export default router;
