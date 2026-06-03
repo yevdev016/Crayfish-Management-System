7ba4094301fed31d551c76b53594ead525d35e20
yevdev016 <curaarvey@gmail.com>
2026-06-03 17:02:13 +0800

    fix

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,25 +3,22 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-const db = new pg.Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+const poolConfig = {
     idleTimeoutMillis: 30000,
     max: 10
-});
+};
 
-const connectDb = async () => {
-    try {
-        const client = await db.connect();
-        console.log("Database is connected")
-        client.release();
-    } catch(err) {
-        console.error('Database connection error: ', err);
-    }
+if (process.env.DATABASE_URL) {
+    poolConfig.connectionString = process.env.DATABASE_URL;
+    poolConfig.ssl = { rejectUnauthorized: false };
+} else {
+    Object.assign(poolConfig, {
+        user: process.env.PG_USER,
+        host: process.env.PG_HOST,
+        database: process.env.PG_DATABASE,
+        password: process.env.PG_PASSWORD,
+        port: process.env.PG_PORT,
+    });
 }
-connectDb();
 
-export default db;
+const db = new pg.Pool(poolConfig);
